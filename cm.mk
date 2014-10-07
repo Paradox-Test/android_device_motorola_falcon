d341ddea9ee71b6e51846414d069e6db0f935bd9
Paradox-Test <sahil.munawar@gmail.com>
2014-10-07 00:47:00 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common paradox stuff.
+$(call inherit-product, vendor/par/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_falcon
+PRODUCT_NAME := par_falcon
